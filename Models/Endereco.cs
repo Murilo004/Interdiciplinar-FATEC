91fa1ae7f39ab80050cde2abb60c9d2374ad9ae5
Murilo Sonsin Ralio <muriloralio8@gmail.com>
2026-07-31 22:35:05 -0300

    Altera as propriedades Cep e Numero para string.

--- Models/Endereco.cs
+++ Models/Endereco.cs
@@ -3,11 +3,11 @@ namespace Interdisciplinar.Models
     public class Endereco
     {
         public int Id { get; set; }
-        public int CEP { get; set; }
+        public string CEP { get; set; }
         public string Cidade { get; set; }
         public string Bairro { get; set; }
         public string Rua { get; set; }
-        public int Numero { get; set; }
+        public string Numero { get; set; }
         public Pessoa Pessoa { get; set; }
         public int PessoaId { get; set; }
     }
